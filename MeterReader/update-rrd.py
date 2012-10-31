7893f6317cd0540f96f9486977c98147326e3dce
Matt Brown <matt@mattb.net.nz>
2012-10-31 00:04:48 +0000

    Improve script for inserting historical RRD data. * Track battery voltage in the RRD as well. * Store data with a 60s interval, 5min allowance for missing data. This seems   to give a pretty good balance between detailed graphs and report frequency. * Make script deal with old and new report formats (counter changed from byte   to long)

--- MeterReader/update-rrd.py
+++ MeterReader/update-rrd.py
@@ -5,12 +5,17 @@
 # All rights reserved.
 #
 # Example RRD commands for use with this script:
-# rrdtool create /tmp/test.rrd -b 1351378113 -s 300 \
-#    DS:kWh:COUNTER:300:U:U RRA:AVERAGE:0.5:1:2628000 RRA:AVERAGE:0.5:5:525600
+# rrdtool create /tmp/test.rrd -b 1351378113 -s 60 \
+#    DS:kWh:COUNTER:300:U:U DS:bat:GAUGE:300:U:U \
+#    RRA:AVERAGE:0.5:1:2628000 RRA:AVERAGE:0.5:5:525600
 # rrdtool graph /tmp/test.png --start end-48h --lower-limit 0 \
 #    --title "Electrical Power Usage" DEF:power=/tmp/test.rrd:kWh:AVERAGE \
 #    --vertical-label Watts --width 800 --height 600 \
 #    CDEF:watts=power,21600,\* LINE1:watts#ff0000:Power && eog /tmp/test.png
+# rrdtool graph /tmp/test.png --start end-48h --lower-limit 0 \
+#    --title "Battery voltage" DEF:bat=/tmp/test.rrd:bat:AVERAGE \
+#    --vertical-label mV --width 800 --height 600 CDEF:mv=bat,50,\+,20,\* \
+#    LINE1:mv#ff0000:Voltage && eog /tmp/test.png
 #
 # Reads logger.py output and generates rrd updates.
 import os
@@ -19,14 +24,28 @@ import time
 
 STEP_SIZE = 300
 
-def get_ping_id(parts):
-  ping_id = 0
+def parse_long(parts, offset):
+  val = 0
   for byte in xrange(0, 4):
-    ping_id += int(parts[3+byte]) << (8*byte)
-  return ping_id
+    val += int(parts[offset+byte]) << (8*byte)
+  return val
 
-def update_rrd(ts, counter):
-  cmd = 'rrdtool update %s %s:%s' % (sys.argv[2], int(ts), counter)
+def parse_line(line):
+  parts = line.strip().split(' ')
+  ts = float(parts[0])
+  ping_id = parse_long(parts, 3)
+  bat = int(parts[8])
+  if len(parts) == 10:
+    # Old format, single byte counter.
+    counter = int(parts[9])
+  elif len(parts) == 13:
+    # New format, long counter.
+    counter = parse_long(parts, 9)
+  return ts, ping_id, counter, bat
+
+
+def update_rrd(ts, counter, bat):
+  cmd = 'rrdtool update %s %s:%s:%s' % (sys.argv[2], int(ts), counter, bat)
   #print time.ctime(ts), cmd
   os.system(cmd)
 
@@ -45,40 +64,46 @@ def main():
     if ' OK ' not in line:
       continue
     parts = line.strip().split(' ')
-    ts = float(parts[0])
-    ping_id = get_ping_id(parts)
-    counter = int(parts[9])
+    ts, ping_id, counter, bat = parse_line(line)
     if lastline:
-      last_ts = float(lastline[0])
+      last_ts, last_ping, last_counter, last_bat = parse_line(lastline)
       while last_ts < (ts - (STEP_SIZE + 1)):
         # Make sure rrd gets data as often as it needs.
         last_ts += STEP_SIZE
-        update_rrd(last_ts, realcounter)
+        update_rrd(last_ts, realcounter, last_bat)
 
-      last_ping = get_ping_id(lastline)
-      last_counter = int(lastline[9])
-      if ping_id - 1 != last_ping:
+      if ping_id == 1:
+        # Reboot
+        step = 1
+      elif ping_id - 1 != last_ping:
         if counter > last_counter:
           # Easy, just subtract.
           step = counter - last_counter
         else:
           # Harder, use ping_id to judge missing reports.
           missing = ping_id - last_ping
-          step = (10 * missing) - 4   # 10 counts per report, minus 4 for wrap.
+          if len(parts) < 13:
+            step = (10 * missing) - 4   # 10 counts per report, minus 4 for wrap.
+          else:
+            # Unlikely!
+            step = missing
       else:
         if counter == 0:
-          # Handle wrap.
-          step = 6
+          if len(parts) < 13:
+            # Handle wrap with old-style byte counter.
+            step = 6
+          else:
+            # Handle wrap with new-style long counter.
+            step = 2**32 - last_counter
         else:
           step = counter - last_counter
-          assert step == 10, 'step %s on %s vs %s' % (step, parts, lastline)
       realcounter += step
     else:
       realcounter = first_count = counter
       start_ts = first_ts = ts
       start_counter = counter
-    lastline = parts
-    update_rrd(ts, realcounter)
+    lastline = line
+    update_rrd(ts, realcounter, bat)
     if ts - start_ts > 3600:
       usage = realcounter - start_counter
       print 'Kwh from %s til %s: %.02fkWh' % (time.ctime(start_ts), time.ctime(ts), usage*6/1000.0)
