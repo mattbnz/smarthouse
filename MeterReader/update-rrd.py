52d24642377a7bf52f6389a34a5878113e173f8c
Matt Brown <matt@mattb.net.nz>
2012-11-11 23:31:09 +0000

    Refactor update-rrd.
    * Improve code quality
    * Prepare for handling temperature data too.

--- MeterReader/update-rrd.py
+++ MeterReader/update-rrd.py
@@ -26,41 +26,158 @@ import time
 STEP_SIZE = 60
 latest_update = -1
 
-def parse_long(parts, offset):
-  val = 0
-  for byte in xrange(0, 4):
-    val += int(parts[offset+byte]) << (8*byte)
-  return val
+NODE_HANDLERS = {
+    1: 'ProcessMeterReader',
+    2: 'ProcessTempSensor',
+    3: 'ProcessTempSensor',
+}
 
-def parse_line(line):
-  parts = line.strip().split(' ')
-  ts = float(parts[0])
-  ping_id = parse_long(parts, 3)
-  bat = int(parts[8])
-  if len(parts) == 10:
-    # Old format, single byte counter.
-    counter = int(parts[9])
-  elif len(parts) >= 13:
-    # New format, long counter.
-    counter = parse_long(parts, 9)
-  return ts, ping_id, counter, bat
+class History(object):
 
+  def __init__(self):
+    self.first_count = 0
+    self.first_ts = 0
+    self.last_ts = 0
+    self.start_ts = 0
+    self.start_counter = 0
+    self.realcount = 0
+    self.lastline = None
 
-def update_rrd(ts, counter, bat):
-  if ts < latest_update:
-    return
-  cmd = 'rrdtool update %s %s:%s:%s' % (sys.argv[1], int(ts), counter, bat)
-  #print time.ctime(ts), cmd
-  os.system(cmd)
+class RRDUpdater(object):
 
-def last_update():
-  cmd = ['rrdtool', 'info', sys.argv[1]]
-  output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-  for line in output.split('\n'):
-    if not line.startswith('last_update'):
-      continue
-    return int(line.strip()[14:])
-  return -1
+  def __init__(self, rrdfile):
+    self.rrdfile = rrdfile
+    self.latest_update = self.GetLastRRDUpdate()
+    self.history = {}
+
+  def ParseLong(self, parts, offset):
+    val = 0
+    for byte in xrange(0, 4):
+      val += int(parts[offset+byte]) << (8*byte)
+    return val
+
+  def ParseMeterLine(self, parts):
+    ts = float(parts[0])
+    ping_id = self.ParseLong(parts, 3)
+    bat = int(parts[8])
+    if len(parts) == 10:
+      # Old format, single byte counter.
+      counter = int(parts[9])
+    elif len(parts) >= 13:
+      # New format, long counter.
+      counter = self.ParseLong(parts, 9)
+    return ts, ping_id, counter, bat
+
+  def UpdateRRD(self, ts, counter, bat):
+    if ts < self.latest_update:
+      return
+    cmd = 'rrdtool update %s %s:%s:%s' % (self.rrdfile, int(ts), counter, bat)
+    #print time.ctime(ts), cmd
+    os.system(cmd)
+
+  def GetLastRRDUpdate(self):
+    cmd = ['rrdtool', 'info', self.rrdfile]
+    output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+    for line in output.split('\n'):
+      if not line.startswith('last_update'):
+        continue
+      return int(line.strip()[14:])
+    return -1
+
+  def GetOrCreateNodeHistory(self, node_id):
+    if node_id not in self.history:
+      self.history[node_id] = History()
+    return self.history[node_id]
+
+  def ProcessFiles(self, files):
+    for filename in files:
+      for line in open(filename, 'r'):
+        parts = line.strip().split(' ')
+        if len(parts) < 3:
+          continue
+        if parts[1] != 'OK':
+          continue
+        node_id = int(parts[2])
+        handler = getattr(self, NODE_HANDLERS.get(node_id, 'IgnoreLine'))
+        handler(node_id, parts)
+
+  def IgnoreLine(self, parts):
+    pass
+
+  def ProcessTempSensor(self, node_id, parts):
+    pass
+
+  def CalculateStep(self, ping_id, counter, last_counter, last_ping, len_parts):
+    if ping_id == 1 or counter < last_counter:
+      # Reboot
+      return 1
+
+    if ping_id - 1 != last_ping:
+      if counter >= last_counter:
+        # Easy, just subtract.
+        return counter - last_counter
+      
+      # Harder, use ping_id to judge missing reports.
+      missing = ping_id - last_ping
+      if len_parts < 13:
+        return (10 * missing) - 4   # 10 counts per report, minus 4 for wrap.
+
+      # Unlikely!
+      return missing
+    
+    # Might be a wrap... Check if last counter value was high.
+    if counter == 0:
+      # Handle wrap with old-style byte counter.
+      if len_parts < 13:
+        if last_counter > ((2*8)*0.9):
+          return 6
+
+      # Handle wrap with new-style long counter.
+      if last_counter > ((2**32)*0.9):
+        return 2**32 - last_counter
+
+      # No wrap, just reset.
+      return 0
+    
+    # Simple case last. Just trust the report.
+    return counter - last_counter
+
+  def ProcessMeterReader(self, node_id, parts):
+    try:
+      ts, ping_id, counter, bat = self.ParseMeterLine(parts)
+    except:
+      return
+    hist = self.GetOrCreateNodeHistory(node_id)
+    if hist.lastline:
+      last_ts, last_ping, last_counter, last_bat = self.ParseMeterLine(hist.lastline)
+      while last_ts < (ts - (STEP_SIZE + 1)):
+        # Make sure rrd gets data as often as it needs.
+        last_ts += STEP_SIZE
+        self.UpdateRRD(last_ts, hist.realcounter, last_bat)
+
+      hist.realcounter += self.CalculateStep(ping_id, counter, last_counter,
+          last_ping, len(parts))
+    else:
+      hist.realcounter = counter
+      hist.first_count = counter
+      hist.start_ts = ts
+      hist.first_ts = ts
+      hist.start_counter = counter
+    hist.lastline = parts
+    hist.last_ts = ts
+    self.UpdateRRD(ts, hist.realcounter, bat)
+    if ts - hist.start_ts > 3600:
+      usage = hist.realcounter - hist.start_counter
+      print 'Kwh from %s til %s: %.02fkWh' % (
+        time.ctime(hist.start_ts), time.ctime(ts), usage*6/1000.0)
+      hist.start_ts = ts
+      hist.start_counter = hist.realcounter
+
+  def PrintMeterSummary(self):
+    hist = self.GetOrCreateNodeHistory(1)
+    usage = hist.realcounter - hist.first_count
+    print 'Kwh from %s til %s: %.02fkWh' % (
+      time.ctime(hist.first_ts), time.ctime(hist.last_ts), usage*6/1000.0)
 
 def main():
   global latest_update
@@ -69,73 +186,9 @@ def main():
         sys.argv[0])
     sys.exit(1)
   
-  latest_update = last_update()
-  first_count = 0
-  first_ts = 0
-  start_ts = 0
-  start_counter = 0
-  realcount = 0
-  lastline = None
-  for filename in sys.argv[2:]:
-    for line in open(filename, 'r'):
-      if ' OK ' not in line:
-        continue
-      parts = line.strip().split(' ')
-      try:
-        ts, ping_id, counter, bat = parse_line(line)
-      except:
-        continue
-      if lastline:
-        last_ts, last_ping, last_counter, last_bat = parse_line(lastline)
-        while last_ts < (ts - (STEP_SIZE + 1)):
-          # Make sure rrd gets data as often as it needs.
-          last_ts += STEP_SIZE
-          update_rrd(last_ts, realcounter, last_bat)
-
-        if ping_id == 1 or counter < last_counter:
-          # Reboot
-          step = 1
-        elif ping_id - 1 != last_ping:
-          if counter >= last_counter:
-            # Easy, just subtract.
-            step = counter - last_counter
-          else:
-            # Harder, use ping_id to judge missing reports.
-            missing = ping_id - last_ping
-            if len(parts) < 13:
-              step = (10 * missing) - 4   # 10 counts per report, minus 4 for wrap.
-            else:
-              # Unlikely!
-              step = missing
-        else:
-          if counter == 0:
-            step = 0
-            # Might be a wrap... Check if last counter value was high.
-            if len(parts) < 13:
-              if last_counter > ((2*8)*0.9):
-                # Handle wrap with old-style byte counter.
-                step = 6
-            else:
-              if last_counter > ((2**32)*0.9):
-                # Handle wrap with new-style long counter.
-                step = 2**32 - last_counter
-          else:
-            step = counter - last_counter
-        #print ping_id, last_ping, counter, last_counter
-        realcounter += step
-      else:
-        realcounter = first_count = counter
-        start_ts = first_ts = ts
-        start_counter = counter
-      lastline = line
-      update_rrd(ts, realcounter, bat)
-      if ts - start_ts > 3600:
-        usage = realcounter - start_counter
-        print 'Kwh from %s til %s: %.02fkWh' % (time.ctime(start_ts), time.ctime(ts), usage*6/1000.0)
-        start_ts = ts
-        start_counter = realcounter
-  usage = realcounter - first_count
-  print 'Kwh from %s til %s: %.02fkWh' % (time.ctime(first_ts), time.ctime(ts), usage*6/1000.0)
+  updater = RRDUpdater(sys.argv[1])
+  updater.ProcessFiles(sys.argv[2:])
+  updater.PrintMeterSummary()
 
 if __name__ == "__main__":
   main()
